fd3ca260cd7964179660fe8dc292e791fa8d5012
Gabriela Teixeira <gabriela.teixeira92@gmail.com>
2023-02-03 20:33:02 -0300

    Modificação em erro no html

--- main.js
+++ main.js
@@ -13,17 +13,15 @@ function adicionaLinha() {
     const inputNome = document.getElementById('nome');
     const inputNumero = document.getElementById('numero');
 
-    if (numeros.includes(inputNumero.value)){
-        alert(`O número: ${inputNumero.value} já foi inserido`);
-    } else {
-        numeros.push(parseInt(inputNumero.value));
-        let linha = '<tr>';
-        linha += `<td>${inputNome.value}</td>`;
-        linha += `<td>${inputNumero.value}</td>`;
-        linha += '</tr>';
+        
+    numeros.push(parseInt(inputNumero.value));
+    let linha = '<tr>';
+    linha += `<td>${inputNome.value}</td>`;
+    linha += `<td>${inputNumero.value}</td>`;
+    linha += '</tr>';
 
-        linhas += linha;
-    }
+    linhas += linha;
+
 
     inputNome.value = '';
     inputNumero.value = '';
